9d24852218c13db7ee543d7594f9e4b194660269
nooribrahim06 <noorribrrahim2006s@gmail.com>
2026-09-22 19:07:50 +0300

    adding the client and prisma files

--- src/lib/prisma.js
+++ src/lib/prisma.js
@@ -1,8 +1,8 @@
 import "dotenv/config";
 import { PrismaPg } from "@prisma/adapter-pg";
-import { PrismaClient } from "../../generated/prisma/client";
+import { PrismaClient } from "@prisma/client";
 
-const connectionString = process.env["DATABASE_URL"];
+const connectionString = process.env.DATABASE_URL;
 
 if (!connectionString) {
   throw new Error("DATABASE_URL is not configured");
